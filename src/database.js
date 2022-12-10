646988d7a87a0a0658506924e622a771037a66ab
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2022-12-10 10:10:56 +0100

    this is how we generate our shit.

--- src/database.js
+++ src/database.js
@@ -1,11 +1,20 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, addDoc, collection } from 'firebase/firestore';
+import { getFirestore, addDoc, collection, getDocs } from 'firebase/firestore';
 import config from '../db_config.js';
 
 const app = initializeApp(config);
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
-async function addData(data) {
+export async function addData(data) {
   const docRef = await addDoc(collection(db, 'myData'), data);
   console.log('Document written with ID: ', docRef.id);
+}
+
+export async function getData() {
+  const data = await getDocs(collection(db, 'myData'));
+  const result = [];
+  for (const doc of data.docs) {
+    result.push(doc.data());
+  }
+  return result;
 }
